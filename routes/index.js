7e4c2fa9b63c9482fe45b7567a713f88b78f7a54
Judy Lin <judith.s.lin@gmail.com>
2015-10-03 21:05:17 -0600

    amazon api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var route = require('./../mongo/index.js');
+amazon = require('amazon-product-api');
+
+var client = amazon.createClient({
+  awsId: process.env.AMAZON_ACCESS_KEY_ID,
+  awsSecret: process.env.AMAZON_SECRET_ACCESS_KEY,
+  awsTag: process.env.AMAZON_SECRET
+});
 
 router.get('/', function (req, res, next) {
   route.allCategories().then(function (categories) {
-    res.render('categories/index', {cookieId: req.session.username, categories: categories})
+    res.render('categories/index', {
+      cookieId: req.session.username,
+      categories: categories
+    })
+  })
+})
+
+router.get('/amazon', function(req, res, next) {
+  res.render('amazon', {
+    cookieId: req.session.username
+  })
+})
+
+router.post('/amazon', function(req, res, next) {
+  client.itemSearch({
+    searchindex: req.body.searchIndex,
+    keywords: req.body.keywords,
+    responseGroup: 'ItemAttributes,Offers,Images'
+  }).then(function(results) {
+    res.render('amazon', {
+      results: results,
+      cookieId: req.session.username
+    })
   })
 })
 
